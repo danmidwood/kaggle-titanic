49393e3bd68653da0cb9b8eaf501749c080b9a25
Dan Midwood <dan@danmidwood.com>
2014-05-11 01:25:29 -0400

    Save the women
    The prediction now is that all women live whereas the men still die.

--- getting-started.r
+++ getting-started.r
@@ -1,12 +1,14 @@
 # Following instructions in Trevor Stevens Getting Started with R series
 #
-# http://trevorstephens.com/post/72918760617/titanic-getting-started-with-r-part-1-booting-up-r
-#
 
 train <- read.csv('train.csv')
 test <- read.csv('test.csv')
 
-test$Survived <- rep(0,nrow(test))
+#summary(train$Sex)
+#prop.table(table(train$Sex,train$Survived),1)
+
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
 
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "guess.csv", row.names = FALSE)
