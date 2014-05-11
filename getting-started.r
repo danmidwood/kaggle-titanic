dcd025ee389b8a615e6f0a97535fae73ed83462c
Dan Midwood <dan@danmidwood.com>
2014-05-11 17:55:11 -0400

    Base decision tree on manually curated data
    In here we split the passenger names values to acquire titles and
    surnames. Rarer titles are munged together with others that are
    similarly-classed.
    
    Afterwards the surnames and family size are used to group passengers
    into families (FamilyID), with suspect data obliterated.

--- getting-started.r
+++ getting-started.r
@@ -11,18 +11,34 @@ library(RColorBrewer)
 train <- read.csv('train.csv')
 test <- read.csv('test.csv')
 
-library(rpart)
+test$Survived <- NA
+combi <- rbind(train, test)
+combi$Name <- as.character(combi$Name)
 
-fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train,
-             method="class", control=rpart.control(minsplit=5, cp=0))
+combi$Title <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+combi$Title <- sub(' ', '', combi$Title)
 
-new.fit <- snip.rpart(fit, toss = 33)
-new.fit <- snip.rpart(new.fit, toss = 129)
-new.fit <- snip.rpart(new.fit, toss = 156)
-new.fit <- snip.rpart(new.fit, toss = 106)
+combi$Title[combi$Title %in% c('Mme', 'Mlle')] <- 'Mlle'
+combi$Title[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+combi$Title[combi$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+combi$Title <- factor(combi$Title)
+combi$FamilySize <- combi$SibSp + combi$Parch + 1
+combi$Surname <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+combi$FamilyID <- paste(as.character(combi$FamilySize), combi$Surname, sep="")
+combi$FamilyID[combi$FamilySize <= 2] <- 'Small'
 
-fancyRpartPlot(new.fit)
+famIDs <- data.frame(table(combi$FamilyID))
+famIDs <- famIDs[famIDs$Freq <= 2,]
+combi$FamilyID[combi$FamilyID %in% famIDs$Var1] <- 'Small'
+combi$FamilyID <- factor(combi$FamilyID)
 
-Prediction <- predict(new.fit, test, type = "class")
+train <- combi[1:891,]
+test <- combi[892:1309,]
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID, data=train, method="class")
+
+fancyRpartPlot(fit)
+
+Prediction <- predict(fit, test, type = "class")
 submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "guess.csv", row.names = FALSE)
