31fe09e88dfb919af167868e6fdd4cf1ffcceac4
Dan Midwood <dan@danmidwood.com>
2014-05-11 11:58:47 -0400

    Prediction by decision tree

--- getting-started.r
+++ getting-started.r
@@ -1,31 +1,22 @@
 # Following instructions in Trevor Stevens Getting Started with R series
 #
 
+#install.packages('rattle')
+#install.packages('rpart.plot')
+#install.packages('RColorBrewer')
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
 train <- read.csv('train.csv')
 test <- read.csv('test.csv')
 
-#summary(train$Age)
+library(rpart)
 
-train$Child <- 0
-train$Child[train$Age < 18] <- 1
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class")
 
-#table(train$Child)
+fancyRpartPlot(fit)
 
-#aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
-#aggregate(Survived ~ Child + Sex, data=train, FUN=length)
-#aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
-
-
-train$Fare2 <- '30+'
-train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
-train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
-train$Fare2[train$Fare < 10] <- '<10'
-
-#aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
-
-test$Survived <- 0
-test$Survived[test$Sex == 'female'] <- 1
-test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
-
-submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+Prediction <- predict(fit, test, type = "class")
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "guess.csv", row.names = FALSE)
