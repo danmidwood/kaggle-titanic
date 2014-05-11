f51213dfb1d465c84392db53b1e68757b8652dc2
Dan Midwood <dan@danmidwood.com>
2014-05-11 01:42:10 -0400

    Improve guess based on class and fare

--- getting-started.r
+++ getting-started.r
@@ -4,11 +4,28 @@
 train <- read.csv('train.csv')
 test <- read.csv('test.csv')
 
-#summary(train$Sex)
-#prop.table(table(train$Sex,train$Survived),1)
+#summary(train$Age)
+
+train$Child <- 0
+train$Child[train$Age < 18] <- 1
+
+#table(train$Child)
+
+#aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
+#aggregate(Survived ~ Child + Sex, data=train, FUN=length)
+#aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+
+#aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
 
 test$Survived <- 0
 test$Survived[test$Sex == 'female'] <- 1
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
 
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "guess.csv", row.names = FALSE)
